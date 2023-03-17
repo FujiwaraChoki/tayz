21171f9fd9d8994e3eff62e18f7b48c989f7d086
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2023-03-17 22:25:51 +0100

    s

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const withRuntimeDotenv = require('next-runtime-dotenv');
-
-module.exports = withRuntimeDotenv({
-    publicRuntimeConfig: {
-        YOUTUBE_API_KEY: process.env.API_KEY,
-    },
-});
