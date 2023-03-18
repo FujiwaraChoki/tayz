e968ff4ce65b8fc6e938cb4ef215e60bfbe95403
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2023-03-18 22:53:47 +0100

    sdgsdfsdgsdfgsdfsddddddddddddddddddddddddddddddddddddddddd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['img.youtube.com', 'yt3.ggpht.com', 'www.pngmart.com'],
   },
